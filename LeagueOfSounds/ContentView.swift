5ed44d8db4d813fe3d276253bb50d9a860053dbd
AK <ak.ramadugu@gmail.com>
2020-07-24 13:19:58 -0400

    Initial Commit

---
+++ LeagueOfSounds/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LeagueOfSounds
+//
+//  Created by Akshith Ramadugu on 7/24/20.
+//  Copyright © 2020 AK-Vitae. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
