e24b611b48108c4619c180c7dfae953e598f1306
AK <ak.ramadugu@gmail.com>
2020-07-25 23:37:07 -0400

    Added codable model that will hold version data from api call

---
+++ LeagueOfSounds/Models/ClientVersion.swift
@@ -0,0 +1,23 @@
+//
+//  DataVersion.swift
+//  LeagueOfSounds
+//
+//  Created by AK on 7/24/20.
+//  Copyright © 2020 AK-Vitae. All rights reserved.
+//
+
+import Foundation
+
+struct ClientVersion: Codable, Identifiable {
+    var id = UUID()
+    var patch: String
+    
+    enum CodingKeys: String, CodingKey {
+        case patch = "v"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.patch = try container.decode(String.self, forKey: .patch)
+    }
+}
