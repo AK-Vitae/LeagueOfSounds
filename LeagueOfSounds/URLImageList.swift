baf4674d0d9ca28f858b4be09e7cc41e0068150b
AK <ak.ramadugu@gmail.com>
2020-08-26 10:24:42 -0500

    Added URLImage package and tested both single image and a list

---
+++ LeagueOfSounds/URLImageList.swift
@@ -0,0 +1,45 @@
+//
+//  URLImageList.swift
+//  LeagueOfSounds
+//
+//  Created by Akshith Ramadugu on 8/26/20.
+//  Copyright © 2020 AK-Vitae. All rights reserved.
+//
+
+import SwiftUI
+import URLImage
+
+struct ListView : View {
+
+    let urls: [URL]
+
+    var body: some View {
+        NavigationView {
+            List(urls, id: \.self) { url in
+                NavigationLink(destination: DetailView(url: url)) {
+                    HStack {
+                        URLImage(url,
+                            delay: 0.25,
+                            processors: [ Resize(size: CGSize(width: 100.0, height: 100.0), scale: UIScreen.main.scale) ],
+                            content:  {
+                                $0.image
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .clipped()
+                            })
+                                .frame(width: 100.0, height: 100.0)
+
+                        Text("\(url)")
+                    }
+                }
+            }
+            .navigationBarTitle(Text("Champions"))
+        }
+    }
+}
+
+struct URLImageList_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView(urls: [URL(string: "https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/Aatrox.png")!, URL(string: "https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/Aatrox.png")!])
+    }
+}
