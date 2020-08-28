c92eb072e3cf2fe7364e8f25b8e7b01a0d760dd5
AK <ak.ramadugu@gmail.com>
2020-08-28 09:31:23 -0500

    Added view for champion abilities that is currently using a fixed url link

--- LeagueOfSounds/DetailView.swift
+++ LeagueOfSounds/DetailView.swift
@@ -10,36 +10,40 @@ import SwiftUI
 import URLImage
 
 struct DetailView : View {
-
+    
     let url: URL
-
+    
     var body: some View {
-        URLImage(url,
-            placeholder: {
-                ProgressView($0) { progress in
-                    ZStack {
-                        if progress > 0.0 {
-                            CircleProgressView(progress).stroke(lineWidth: 8.0)
+        VStack{
+            URLImage(url,
+                     placeholder: {
+                        ProgressView($0) { progress in
+                            ZStack {
+                                if progress > 0.0 {
+                                    CircleProgressView(progress).stroke(lineWidth: 8.0)
+                                }
+                                else {
+                                    CircleActivityView().stroke(lineWidth: 50.0)
+                                }
+                            }
                         }
-                        else {
-                            CircleActivityView().stroke(lineWidth: 50.0)
-                        }
-                    }
-                }
-                    .frame(width: 50.0, height: 50.0)
+                        .frame(width: 50.0, height: 50.0)
             },
-            content: {
-                $0.image
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 100, height: 100)
-                    .clipped()
-                    .clipShape(RoundedRectangle(cornerRadius: 5))
-                    .padding(.all, 40.0)
-                    .shadow(radius: 10.0)
-                    .onTapGesture {
-                        playSound()
-                }
+                     content: {
+                        $0.image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 100, height: 100)
+                            .clipped()
+                            .clipShape(RoundedRectangle(cornerRadius: 5))
+                            .padding(.all, 40.0)
+                            .shadow(radius: 10.0)
+                        //                    .onTapGesture {
+                        //                        playSound()
+                        //                }
             })
+            AbilitiesView(url: URL(string: "https://ddragon.leagueoflegends.com/cdn/10.16.1/img/spell/FlashFrost.png")!)
+            Spacer()
+        }
     }
 }
