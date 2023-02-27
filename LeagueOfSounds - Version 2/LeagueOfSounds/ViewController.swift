59bf46b2237416e597dffd5802bb37655fb401cc
AK <ak.ramadugu@gmail.com>
2023-02-27 08:56:05 -0500

    Added UIKit centric version of app with programmatic UI setup

---
+++ LeagueOfSounds - Version 2/LeagueOfSounds/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LeagueOfSounds
+//
+//  Created by Akshith Ramadugu on 2/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+    }
+
+
+}
+
